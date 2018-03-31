3c54a262267d0f2c7b244d110200f73a96b0a276
Sebastian Mulinek <s12102@pjwstk.edu.pl>
2018-03-31 20:49:03 +0200

    Initial commit

--- Encrypt.py
+++ Encrypt.py
@@ -5,29 +5,86 @@ import os
 from Crypto import Random
 from Crypto.Cipher import AES
 
+
 class AESCipher(object):
     def __init__(self, key):
         self.bs = 16
-        self.key = hashlib.sha256(key.encode('utf-8')).digest()
-    
-    def genKey(self):
-        return os.urandom(256)
+        self.globalKey = hashlib.sha256(key.encode('utf-8')).digest()
 
-    def encrypt(self, raw):
-        #zmiany
-        hashKey = hashlib.sha256(self.genKey()).digest()
+    def genKey(self):
+        return os.urandom(1024)
+
+    def encrypt(self, key, raw):
+
         raw = self.pad(raw)
         iv = Random.new().read(AES.block_size)
-        cipher = AES.new(hashKey, AES.MODE_CBC, iv)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(raw))
 
-    def decrypt(self, enc):
-        #zmiany
+    def encryptFile(self, path):
         hashKey = hashlib.sha256(self.genKey()).digest()
+        encryptKey = self.encrypt(self.globalKey, hashKey)
+        print(os.stat(path).st_size)
+        # print(os.stat('decoded.jpg').st_size)
+        size = self.encrypt(hashKey, str(os.stat(path).st_size).encode())
+        name = self.encrypt(hashKey, os.path.basename(path).encode())
+        basename = os.path.splitext(os.path.basename(path))[0]
+        string = ''
+        print(hashKey)
+
+        with open(path, "rb") as file:
+            flag = True
+            while True:
+                byte = file.read(1)
+                if not byte:
+                    break
+                if flag:
+                    string = str(ord(byte))
+                    flag = False
+                else:
+                    string += ";" + str(ord(byte))
+        encryptString = self.encrypt(hashKey, string.encode())
+
+        with open(str(basename + '.encrypt'), 'w') as f:
+            string = encryptKey.decode() + '\n' + \
+                     size.decode() + '\n' + \
+                     name.decode() + '\n' + \
+                     encryptString.decode()
+            f.write(string)
+            print("Encrypt of a file success")
+
+    def decrypt(self, key, enc):
+        # zmiany
         enc = base64.b64decode(enc)
         iv = enc[:16]
-        cipher = AES.new(hashKey, AES.MODE_CBC, iv)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
         return self.unpad(cipher.decrypt(enc[16:]))
+
+    def decryptFile(self, path):
+
+        file = open(path, "r")
+        encryptArray = file.readlines()
+        # print("Name of the file: ", fo.name)
+        hashKey = self.decrypt(self.globalKey, encryptArray[0].strip('\n').encode())
+        size = self.decrypt(hashKey, encryptArray[1].strip('\n').encode())
+        name = self.decrypt(hashKey, encryptArray[2].strip('\n').encode())
+        decodeString = self.decrypt(hashKey, encryptArray[3].encode()).decode().split(';')
+
+        print(hashKey)
+        parse = [int(i) for i in decodeString]
+        decPath = name.decode().replace('test2', 'decoded')
+        newFile = open(decPath, "wb")
+        # write to file
+        for byte in parse:
+            newFile.write(byte.to_bytes(1, byteorder='big'))
+
+        print()
+        print(size.decode(), os.stat(decPath).st_size)
+        print("Decode of a file sucess")
+        print()
+        print(os.stat('decoded.jpg').st_size)
+        print(os.path.getsize('decoded.jpg'))
+        print(os.path.getsize('test2.jpg'))
 
     def pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs).encode()
--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import os
+
 from Encrypt import AESCipher
 
 print("Hello world")
@@ -7,51 +9,61 @@ file = []
 
 
 if __name__ == '__main__':
-
-    with open("test2.jpg", "rb") as f:
-        # byte = f.read(1)
-        while True:
-            # Do stuff with byte.
-            byte = f.read(1)
-            if not byte:
-                break
-            # print(ord(byte))
-            file.append(ord(byte))
-            # print(file)
-
-    print(file)
-
     key = 'siemakurwozajebanawdupe4567624//dsa5'
+    cipher = AESCipher(key)
 
-    cipher = AESCipher()
-    # key = cipher.genKey()
-    print(key)
-    text = ''
-    flag = True
-    for x in file:
-        if flag:
-            text = str(x)
-            flag = False
-        else:
-            text += ";" + str(x)
+    cipher.encryptFile('test2.jpg')
+    cipher.decryptFile('test2.encrypt')
 
-    print(text)
-    encrypted = cipher.encrypt(key, text.encode())
-    print(encrypted)
 
-    decrypted = cipher.decrypt(key, encrypted)
-    print("decrypt")
-    print(decrypted.decode())
-    print(decrypted.decode() == text)
-
-    final = decrypted.decode().split(';')
-    results = [int(i) for i in final]
-    print(results)
-
-    with open('test.encrypt', 'w') as f:
-        f.write(encrypted.decode())
-
-    newFile = open("decrypt.jpg", "wb")
-    # write to file
-    for byte in results:
-        newFile.write(byte.to_bytes(1, byteorder='big'))
+    # with open("test2.jpg", "rb") as f:
+    #     # byte = f.read(1)
+    #     print(os.stat('test2.jpg').st_size)
+    #     # os.remove('test2 — kopia.jpg')
+    #     while True:
+    #         # Do stuff with byte.
+    #         byte = f.read(1)
+    #         if not byte:
+    #             break
+    #         # print(ord(byte))
+    #         file.append(ord(byte))
+    #         # print(file)
+    #
+    # print(file)
+    #
+    # key = 'siemakurwozajebanawdupe4567624//dsa5'
+    #
+    # cipher = AESCipher(key)
+    # # key = cipher.genKey()
+    # print(key)
+    # text = ''
+    # flag = True
+    # for x in file:
+    #     if flag:
+    #         text = str(x)
+    #         flag = False
+    #     else:
+    #         text += ";" + str(x)
+    #
+    # path = ''
+    #
+    # print(text)
+    # encrypted = cipher.encrypt(path) #, text.encode())
+    # print(encrypted)
+    #
+    # decrypted = cipher.decrypt(key, encrypted)
+    # print("decrypt")
+    # print(decrypted.decode())
+    # print(decrypted.decode() == text)
+    #
+    # final = decrypted.decode().split(';')
+    # results = [int(i) for i in final]
+    # print(results)
+    #
+    # with open('test.encrypt', 'w') as f:
+    #     f.write(encrypted.decode())
+    #
+    # newFile = open("decrypt.jpg", "wb")
+    # # write to file
+    # for byte in results:
+    #     newFile.write(byte.to_bytes(1, byteorder='big'))
